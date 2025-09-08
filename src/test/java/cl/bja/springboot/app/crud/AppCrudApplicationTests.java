b9594a7e92783c9cf9be8256aced1230d1dccd1d
root <root@Aorus.localdomain>
2025-09-08 19:01:22 -0300

    first commit

---
+++ src/test/java/cl/bja/springboot/app/crud/AppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bja.springboot.app.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
