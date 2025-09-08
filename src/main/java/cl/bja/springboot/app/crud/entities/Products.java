5cea7b4fd9bc387d7b138c68f9d98a0b7ff2c3ed
root <root@Aorus.localdomain>
2025-09-08 19:05:59 -0300

    se agrega Procudct entity y su service

---
+++ src/main/java/cl/bja/springboot/app/crud/entities/Products.java
@@ -0,0 +1,49 @@
+package cl.bja.springboot.app.crud.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price;
+    private String description;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/cl/bja/springboot/app/crud/repositories/ProductRepository.java
@@ -0,0 +1,6 @@
+package cl.bja.springboot.app.crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository {
+}
